df08af6eeb9c7fa33a5d9ff4a30ea313a34f8dcc
Rebecca <ftstc451810@gmail.com>
2017-01-05 18:01:12 +0800

    no message

--- app/src/main/java/com/mac/thirthir_git/MainActivity.java
+++ app/src/main/java/com/mac/thirthir_git/MainActivity.java
@@ -95,7 +95,7 @@ public class MainActivity extends Activity {
             tv_set03.setText(REMIND_TIME[2]);
         }
 
-        
+//0105businessbackground
         //12/23土法煉鋼完成區塊操作
 //        RelativeLayout rl_remind_content = (RelativeLayout)findViewById(R.id.rl_remind_content);
 //        TextView tv_remind_stm = (TextView)findViewById(R.id.tv_remind_stm);
