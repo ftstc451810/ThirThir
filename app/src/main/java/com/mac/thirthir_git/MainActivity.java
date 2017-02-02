5e0065588591fb4cca360cb7c73995c8a1a7ad3b
Rebecca <ftstc451810@gmail.com>
2017-02-02 23:12:27 +0800

    no message

--- app/src/main/java/com/mac/thirthir_git/MainActivity.java
+++ app/src/main/java/com/mac/thirthir_git/MainActivity.java
@@ -96,6 +96,8 @@ public class MainActivity extends Activity {
         }
 
         //實作原理、設計模型、行動裝置底層運作方式
+        //happy
+        
         //12/23土法煉鋼完成區塊操作
 //        RelativeLayout rl_remind_content = (RelativeLayout)findViewById(R.id.rl_remind_content);
 //        TextView tv_remind_stm = (TextView)findViewById(R.id.tv_remind_stm);
